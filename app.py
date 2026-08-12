cdd361b1a304882586fbacd232dfc61244588c47
alionehebo <alionehebo05@gmail.com>
2026-08-12 22:13:11 +0100

    configuração da página do streamlit

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import streamlit as st
+
+#configuração da página streamlit
+st.set_page_config(
+    page_title="Análise de Churn",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
